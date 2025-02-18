1e521585b4b663b401402ff9ed088f9da433a45b
Will Lavey <wlavey@middlebury.edu>
2025-02-18 11:02:11 -0500

    working data first pass

--- first-pass-data.R
+++ first-pass-data.R
@@ -7,6 +7,12 @@ library(censusapi)
 
 Sys.setenv(CENSUS_KEY = "d2c6932eca5b04592aaa4b32840c534b274382dc")
 
+state_fips <- 50
+
 test <- getCensus(
-  
+  name = "acs/acs5",
+  vintage = 2020,
+  vars = c("NAME", "B01001_001E", "B19013_001E"),
+  region = "county:*",
+  regionin = paste0("state:", state_fips)
 )
