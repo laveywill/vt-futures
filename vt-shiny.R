05c447517453362c1b8670a96c1f46b96d732f67
carlymcadam <carlymcadam@gmail.com>
2025-03-25 10:54:08 -0400

    app demo finished

--- vt-shiny.R
+++ vt-shiny.R
@@ -10,6 +10,8 @@ library(bslib)
 library(DT)
 
 source("/Users/carlymcadam/Desktop/College/Midd2024-25/Spring2025/STAT711/vt-futures/population.R")
+source("/Users/carlymcadam/Desktop/College/Midd2024-25/Spring2025/STAT711/vt-futures/state_data_functions.R")
+
 Sys.setenv(CENSUS_KEY = "d2c6932eca5b04592aaa4b32840c534b274382dc")
 state_fips <- 50 # VT
 
@@ -58,48 +60,94 @@ vt_counties <- counties(state = "VT", cb = TRUE, class = "sf", year = "2022")
 
 vt_map <- left_join(vt_counties, census_data, by = "NAME")
 
-
-
-
+theme <- bs_theme(
+  primary = "darkgreen", secondary = "#2c3e50",
+  base_font = c("Georgia", "sans-serif"),
+  heading_font = c("Georgia", "sans-serif"),
+  "input-border-color" = "darkgreen"
+)
 
 #### UI #### 
 
 ui <- page_fluid(
+  theme = theme,
   titlePanel("Vermont Futures: Interactive Dashboard"),
   p("This is the main page for the data exploration dashboard. This should be placed right below the title."),
   
   navset_card_pill(
-
     # Population tab is an example layout of what we want
     nav_panel("Population",
       layout_column_wrap(  
         width = 1,
         card(
-          card_header("State Population"),
+          card_header(class = "bg-primary", "State Population"),
           card_body(
-            layout_column_wrap(
-              width = 1/2,
-              textOutput("pop_text"),
-              dataTableOutput("pop_state")
+            sidebarLayout(
+              sidebarPanel(
+                p("In 2000, Vermont had a high proportion of prime working-age adults
+          relative to the number of children and elderly. There were significantly
+          more children than there are today. In 2023 (pictured right), Vermont's
+          working-age population (25–49 years old) is much smaller relative to 
+          the number of children and elderly."),
+                p("The fertility rate is too low to grow the future workforce and tax base. 
+          This demographic shift has led to an imbalance in the workforce, where
+          the supply of working-age individuals is insufficient to meet the demand
+          for labor."),
+                p("As a result, employers struggle to fill positions, which limits productivity 
+          and economic growth. Consumers face reduced access to services like 
+          childcare, dining, repairs, and healthcare."),
+                p("Growing the prime working-age population is essential to closing the 
+          workforce gap, improving affordability, and strengthening communities 
+          to better meet the needs of all Vermonters.")
+              ),
+              mainPanel(
+                plotOutput("age_plot", height = "600px")
+              )
             )
           )
         ),
         card(
-          card_header("County Level Exploration"),
+          card_header(class = "bg-primary", "County Level Exploration"),
           layout_sidebar(
             sidebar = sidebar(
               bg = "lightgrey",
               selectInput("pop_county_col", 
                           label = "Select a Variable to Explore",
-                          choices = census_variables$title)
+                          choices = census_variables$title),
+              sidebarPanel(
+                strong("National Benchmarks"),
+                p("\n"),
+                p("Average capita income: $37,683"),
+                p("Median age: 38.7"), 
+                p("Poverty Rate: 11.1%"), 
+                p("Median home value: $420,000"), 
+                p("Average labor force participation rate: 62%"),
+                , width = "150px"
+              ),
             ),
             plotOutput("pop")
           )
         ),
         card(
-          card_header("Vermont Age Distribution (2023)"),
+          card_header(class = "bg-primary", "County-Level Breakdown of VFP Population Goals"),
           card_body(
-            plotOutput("age_plot", height = "700px")
+            layout_sidebar(
+              sidebar = sidebar(
+                bg = "lightgrey",
+                width = "300px",
+                selectInput("selected_county", "Select a County:", 
+                            choices = NULL, 
+                            selected = NULL),
+                p("VFP has a goal of increasing Vermont’s population to 802,000
+           residents by 2035 by recruiting and retaining working-age people."),
+                p("Here, we can see the population goal for each county compared
+           with its current capacities for adding new population
+           in different areas."),
+                p("It might make sense to add some more description here
+           of the different capacity metrics.")
+              ),
+                plotOutput("county_plot", height = "600px")
+            )
           )
         ),
       )
@@ -118,6 +166,23 @@ ui <- page_fluid(
 
 server <- function(input, output, session) {
   
+  county_caps_df <- reactive({
+    build_county_caps_df()
+  })
+  
+  observe({
+    updateSelectInput(session, "selected_county", 
+                      choices = unique(county_caps_df()$County),
+                      selected = unique(county_caps_df()$County)[1])
+  })
+  
+  filtered_data <- reactive({
+    county_caps_df() %>%
+      filter(County == input$selected_county) %>%
+      pivot_longer(cols = -County, names_to = "Metric", values_to = "Value")
+  })
+  
+  
   output$pop_text <- renderText({
     "This is some test text for the page"
   })
@@ -134,7 +199,47 @@ server <- function(input, output, session) {
       geom_sf(aes(fill = !!as.symbol(input$pop_county_col))) + 
       geom_sf_label(aes(label = !!as.symbol(input$pop_county_col))) +
       geom_sf_label(aes(label = NAME), nudge_y = -0.1) + 
+      scale_fill_gradient(
+        low = "honeydew", 
+        high = "darkgreen", 
+        name = input$pop_county_col
+      ) +
       theme_void()
+  })
+  
+  output$age_plot <- renderPlot({
+    age_data <- age_distribution_data(2023)
+    plot_age_distribution(age_data)
+  })
+  
+  output$county_plot <- renderPlot({
+    ggplot(filtered_data() %>% 
+             mutate(Metric = factor(Metric, levels = c("pop_goal", "latent_cap", "jobs_homes_diff", "latent_cap_school"))),
+           aes(x = Metric, y = Value, fill = Metric)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = paste("County Capacity Limitations:", input$selected_county, "\n"), 
+           x = "\nMetric", y = "Value") +
+      theme_minimal() +
+      scale_x_discrete(labels = c(
+        "latent_cap" = "Latent Capacity",
+        "jobs_homes_diff" = "Jobs-Homes Difference",
+        "latent_cap_school" = "School Latency",
+        "pop_goal" = "Population Goal"
+      )) + 
+      scale_fill_manual(values = c(
+        "latent_cap" = "deepskyblue1",
+        "jobs_homes_diff" = "orange",
+        "latent_cap_school" = "red",
+        "pop_goal" = "aquamarine4"
+      )) +
+      theme(legend.position = "none", 
+            text = element_text(family = "Georgia"),
+            plot.margin = margin(t = 10, r = 10, b = 40, l = 10),
+            axis.title.x = element_text(size = 16, face = "bold"),
+            axis.title.y = element_text(size = 16, face = "bold"),
+            axis.text.x = element_text(size = 14, face = "bold"),
+            axis.text.y = element_text(size = 14, face = "bold"),
+            plot.title = element_text(size = 22, face = "bold"))
   })
   
   output$jobs <- renderDataTable(
