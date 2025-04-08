9fbd82084ac4b41a6bb05b62449af8db3277ccf2
Will Lavey <wlavey@middlebury.edu>
2025-04-08 14:48:39 -0400

    changed labor force data read in functions

--- jobs.R
+++ jobs.R
@@ -139,7 +139,7 @@ plot_prime_working_age <- function(prime_age_df) {
     annotate("text", x = 1, y = 0.84, label = "National Average", hjust = 0, vjust = 2, size = 4, fontface = "italic")
 }
 
-plot_dependency_ratio <- function(dependency_df, title = "Dependency Ratio by County") {
+plot_dependency_ratio <- function(dependency_df) {
   ggplot(dependency_df, aes(x = reorder(NAME, dependency_ratio), y = dependency_ratio, fill = dependency_ratio)) +
     geom_col(width = 0.7) +
     geom_text(aes(label = sprintf("%.2f", dependency_ratio)), 
@@ -148,7 +148,7 @@ plot_dependency_ratio <- function(dependency_df, title = "Dependency Ratio by Co
               color = "black") +
     coord_flip() +
     labs(
-      title = title,
+      title = "Dependency Ratio by County",
       x = "County",
       y = "Dependency Ratio",
       caption = "Note: Dependency ratio = (Age 0–14 + Age 65+) / (Age 16–64)\nSource: American Community Survey (ACS)"
@@ -166,155 +166,3 @@ plot_dependency_ratio <- function(dependency_df, title = "Dependency Ratio by Co
     )
 }
 
-get_lf_data <- function() {
-  age_vars <- paste0("B01001_", sprintf("%03d", 2:49))
-  labor_force_vars <- paste0("B23001_", sprintf("%03d", 1:173))
-  
-  vermont_age_data <- get_acs(
-    geography = "county",
-    state = "VT",
-    variables = age_vars,
-    year = 2020,
-    survey = "acs5"
-  )
-  
-  vermont_lf_age_data <- get_acs(
-    geography = "county",
-    state = "VT",
-    variables = labor_force_vars,
-    year = 2020,
-    survey = "acs5"
-  )
-  
-  acs_vars <- load_variables(2020, "acs5", cache = TRUE)
-  
-  acs_vars <- rename(acs_vars, variable =  name)
-  
-  binned_age_df <- left_join(vermont_age_data, acs_vars, by = "variable") %>%
-    group_by(NAME) %>%
-    summarise(
-      age_0_to_14 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!Under 5 years", "Estimate!!Total:!!Female:!!Under 5 years",
-        "Estimate!!Total:!!Male:!!5 to 9 years", "Estimate!!Total:!!Female:!!5 to 9 years",
-        "Estimate!!Total:!!Male:!!10 to 14 years", "Estimate!!Total:!!Female:!!10 to 14 years"
-      )], na.rm = TRUE),
-      
-      age_16_to_24 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!15 to 17 years", "Estimate!!Total:!!Female:!!15 to 17 years",
-        "Estimate!!Total:!!Male:!!18 and 19 years", "Estimate!!Total:!!Female:!!18 and 19 years",
-        "Estimate!!Total:!!Male:!!20 years", "Estimate!!Total:!!Female:!!20 years",
-        "Estimate!!Total:!!Male:!!21 years", "Estimate!!Total:!!Female:!!21 years",
-        "Estimate!!Total:!!Male:!!22 to 24 years", "Estimate!!Total:!!Female:!!22 to 24 years"
-      )], na.rm = TRUE),
-      
-      age_25_to_34 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!25 to 29 years", "Estimate!!Total:!!Female:!!25 to 29 years",
-        "Estimate!!Total:!!Male:!!30 to 34 years", "Estimate!!Total:!!Female:!!30 to 34 years"
-      )], na.rm = TRUE),
-      
-      age_35_to_44 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!35 to 39 years", "Estimate!!Total:!!Female:!!35 to 39 years",
-        "Estimate!!Total:!!Male:!!40 to 44 years", "Estimate!!Total:!!Female:!!40 to 44 years"
-      )], na.rm = TRUE),
-      
-      age_45_to_54 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!45 to 49 years", "Estimate!!Total:!!Female:!!45 to 49 years",
-        "Estimate!!Total:!!Male:!!50 to 54 years", "Estimate!!Total:!!Female:!!50 to 54 years"
-      )], na.rm = TRUE),
-      
-      age_55_to_64 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!55 to 59 years", "Estimate!!Total:!!Female:!!55 to 59 years",
-        "Estimate!!Total:!!Male:!!60 and 61 years", "Estimate!!Total:!!Female:!!60 and 61 years",
-        "Estimate!!Total:!!Male:!!62 to 64 years", "Estimate!!Total:!!Female:!!62 to 64 years"
-      )], na.rm = TRUE),
-      
-      age_65_and_over = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!65 and 66 years", "Estimate!!Total:!!Female:!!65 and 66 years",
-        "Estimate!!Total:!!Male:!!67 to 69 years", "Estimate!!Total:!!Female:!!67 to 69 years",
-        "Estimate!!Total:!!Male:!!70 to 74 years", "Estimate!!Total:!!Female:!!70 to 74 years",
-        "Estimate!!Total:!!Male:!!75 to 79 years", "Estimate!!Total:!!Female:!!75 to 79 years",
-        "Estimate!!Total:!!Male:!!80 to 84 years", "Estimate!!Total:!!Female:!!80 to 84 years",
-        "Estimate!!Total:!!Male:!!85 years and over", "Estimate!!Total:!!Female:!!85 years and over"
-      )], na.rm = TRUE)
-    )
-  
-  binned_lf_age_df <- left_join(vermont_lf_age_data, acs_vars, by = "variable") %>%
-    group_by(NAME) %>% 
-    summarise(
-      age_16_to_24 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!16 to 19 years:!!In labor force:", "Estimate!!Total:!!Female:!!16 to 19 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!20 and 21 years:!!In labor force:", "Estimate!!Total:!!Female:!!20 and 21 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!22 to 24 years:!!In labor force:", "Estimate!!Total:!!Female:!!22 to 24 years:!!In labor force:"
-      )], na.rm = TRUE),
-      
-      age_25_to_34 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!25 to 29 years:!!In labor force:", "Estimate!!Total:!!Female:!!25 to 29 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!30 to 34 years:!!In labor force:", "Estimate!!Total:!!Female:!!30 to 34 years:!!In labor force:"
-      )], na.rm = TRUE),
-      
-      age_35_to_44 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!35 to 44 years:!!In labor force:", "Estimate!!Total:!!Female:!!35 to 44 years:!!In labor force:"
-      )], na.rm = TRUE),
-      
-      age_45_to_54 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!45 to 54 years:!!In labor force:", "Estimate!!Total:!!Female:!!45 to 54 years:!!In labor force:"
-      )], na.rm = TRUE),
-      
-      age_55_to_64 = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!55 to 59 years:!!In labor force:", "Estimate!!Total:!!Female:!!55 to 59 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!60 and 61 years:!!In labor force:", "Estimate!!Total:!!Female:!!60 and 61 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!62 to 64 years:!!In labor force:", "Estimate!!Total:!!Female:!!62 to 64 years:!!In labor force:"
-      )], na.rm = TRUE),
-      
-      age_65_and_over = sum(estimate[label %in% c(
-        "Estimate!!Total:!!Male:!!65 and 69 years:!!In labor force:", "Estimate!!Total:!!Female:!!65 and 69 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!70 to 74 years:!!In labor force:", "Estimate!!Total:!!Female:!!70 to 74 years:!!In labor force:",
-        "Estimate!!Total:!!Male:!!75 years and over:!!In labor force:", "Estimate!!Total:!!Female:!!75 years and over:!!In labor force:"
-      )], na.rm = TRUE)
-    )
-  
-  pivoted_binned_age_df <- binned_age_df |> 
-    pivot_longer(
-      cols = starts_with("age"),
-      names_to = "age_group", 
-      values_to = "count"
-    )
-  
-  pivoted_binned_lf_age_df <- binned_lf_age_df |> 
-    pivot_longer(
-      cols = starts_with("age"),
-      names_to = "age_group", 
-      values_to = "labor_force_count"
-    )
-  
-  cleaned_age_distribution <- left_join(pivoted_binned_age_df, pivoted_binned_lf_age_df, by = c("NAME", "age_group")) |> 
-    mutate_if(is.numeric,coalesce,0)
-  
-  return(cleaned_age_distribution)
-}
-
-get_prime_age_data <- function(labor_force_df) {
-  df <- labor_force_df %>%
-    filter(age_group== "age_25_to_34"|age_group == "age_35_to_44"|age_group == "age_45_to_54") %>% 
-    group_by(NAME) %>% 
-    summarise(prime_labor_pr = sum(labor_force_count)/sum(count)) %>% 
-    arrange(desc(prime_labor_pr))
-  
-  return(df)
-}
-
-get_dependency_data <- function(labor_force_df) {
-  
-  df <- labor_force_df %>%
-    group_by(NAME) %>%
-    summarise(
-      dependents = sum(count[age_group %in% c("age_0_to_14", "age_65_and_over")], na.rm = TRUE),
-      working_age = sum(count[age_group %in% c(
-        "age_16_to_24", "age_25_to_34", "age_35_to_44", 
-        "age_45_to_54", "age_55_to_64"
-      )], na.rm = TRUE),
-      dependency_ratio = dependents / working_age
-    )
-  
-  return(df)
-}
